31c8e92aab6356e2828f13a7a664d228594f2356
Mahmoud Atef Abouelsoud Ali <mahmoud.atef98@outlook.com>
2020-11-25 23:01:47 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,10 @@
 if($(document).width() <= 980) {
   $(".skills-div").addClass("container-fluid");
   $(".skills-div").removeClass("container");
+
+  $(".certificates-div").addClass("container-fluid");
+  $(".certificates-div").removeClass("container");
+  $(".certificates-div").css("text-align", "center");
+  $(".certificates-text").css("padding", "0 20%");
+  $(".certificates-text").css("margin-top", "15px");
 }
